55bc01f938d7c0a65b329df1c9ce520373d3109f
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2022-04-05 09:13:19 +0800

    feat: initial boilerplate setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,38 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          alias: {
+            "@app/api": "./src/api",
+            "@app/components": "./src/components",
+            "@app/constants": "./src/constants",
+            "@app/hooks": "./src/hooks",
+            "@app/navigation": "./src/navigation",
+            "@app/redux": "./src/redux",
+            "@app/screens": "./src/screens",
+            "@app/types": "./src/types",
+            "@app/utils": "./src/utils",
+          },
+        },
+        "inline-dotenv",
+      ],
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
   };
 };
