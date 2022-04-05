b49c4297cf535676f060ae23dce9f99dcf6ff7c3
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2022-04-05 17:36:39 +0800

    feat: added profile section

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { View } from "react-native";
+import Text from "@app/components/Text";
+
+import type { PropsType } from "./types";
+import ProfileStyles from "./styles";
+
+const Profile: React.FC<PropsType> = (props) => {
+  const { name, nickname } = props;
+
+  return (
+    <View style={ProfileStyles.container}>
+      <View style={ProfileStyles.row}>
+        <Text text={`Github Name: `} textStyle={ProfileStyles.txtLabel} />
+        <Text text={String(name)} textStyle={ProfileStyles.txtValue} />
+      </View>
+      <View style={ProfileStyles.row}>
+        <Text text={`Github URL: `} textStyle={ProfileStyles.txtLabel} />
+        <Text text={String(nickname)} textStyle={ProfileStyles.txtValue} />
+      </View>
+    </View>
+  );
+};
+
+export default Profile;
