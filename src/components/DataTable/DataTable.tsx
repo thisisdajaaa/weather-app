bbaa0b3246b54a435ef4f9b243c590024c75b701
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2022-04-05 17:05:56 +0800

    feat: added data table component

---
+++ src/components/DataTable/DataTable.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { View } from "react-native";
+import Text from "@app/components/Text";
+
+import type { PropsType } from "./types";
+import DataTableStyles from "./styles";
+
+const DataTable: React.FC<PropsType> = (props) => {
+  const { tableData } = props;
+
+  return (
+    <View style={DataTableStyles.table}>
+      {tableData.map((row, tableIndex) => {
+        const header = tableIndex === 0;
+
+        return (
+          <View key={`row-${tableIndex}`} style={DataTableStyles.row}>
+            {row.map((field, rowIndex) => {
+              const dateColumn = rowIndex === 0;
+
+              return (
+                <View
+                  key={`field-${rowIndex}`}
+                  style={[
+                    DataTableStyles.column,
+                    dateColumn
+                      ? DataTableStyles.flexTwo
+                      : DataTableStyles.flexOne,
+                  ]}
+                >
+                  <Text
+                    text={String(field)}
+                    textStyle={
+                      header ? DataTableStyles.headers : DataTableStyles.field
+                    }
+                  />
+                </View>
+              );
+            })}
+          </View>
+        );
+      })}
+    </View>
+  );
+};
+
+export default DataTable;
---
+++ src/components/DataTable/types/index.ts
@@ -0,0 +1,3 @@
+export type PropsType = {
+  tableData: (string | null)[][];
+};
